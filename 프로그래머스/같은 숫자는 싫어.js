259b29df3feea457d3cd474faee70230996ec75a
reason <reasonz@daum.net>
2022-09-02 14:53:41 +0900

    프로그래머스 같은 숫자는 싫어

---
+++ 프로그래머스/같은 숫자는 싫어.js
@@ -0,0 +1,14 @@
+function solution(arr) {
+  var answer = [];
+  answer.push(arr[0]);
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] == arr[i + 1]) {
+      continue;
+    } else {
+      answer.push(arr[i + 1]);
+    }
+  }
+
+  return answer;
+}
