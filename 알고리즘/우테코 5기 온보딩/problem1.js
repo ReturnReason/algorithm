d80596e9a2c922ccd433427beb9dcd5929e33ea7
ReturnReason <reasonz@daum.net>
2022-11-24 19:41:09 +0900

    feat: 예외처리만 담당하는 함수 추가

--- 알고리즘/우테코 5기 온보딩/problem1.js
+++ 알고리즘/우테코 5기 온보딩/problem1.js
@@ -1,11 +1,66 @@
 const problem1 = (pobi, crong) => {
   const [pobiLeftPage, pobiRightPage] = pobi;
   const [crongLeftPage, crongRightPage] = crong;
-
-  // 반환값이 false이면 결과 -1로 리턴하기
 };
 
-// 예외처리
+// 예외처리 번들
+const validCheckBundle = ([
+  pobiLeftPage,
+  pobiRightPage,
+  crongLeftPage,
+  crongRightPage,
+]) => {
+  // 왼쪽 페이지가 홀수인지 확인한다.
+  if (!checkOdd(pobiLeftPage) || !checkOdd(crongLeftPage)) {
+    return false;
+  }
+
+  // 오른쪽 페이지가 짝수인지 확인한다.
+  if (!checkEven(pobiRightPage) || !checkEven(crongRightPage)) {
+    return false;
+  }
+
+  // 페이지가 비어있는지 확인한다.
+  if (
+    isEmptyPages(pobiLeftPage, pobiRightPage) ||
+    isEmptyPages(crongLeftPage, crongRightPage)
+  ) {
+    return false;
+  }
+
+  // 페이지가 범위를 벗어났는지 확인한다.
+  if (
+    isOutOfRangePages(pobiLeftPage, pobiRightPage) ||
+    isOutOfRangePages(crongLeftPage, crongRightPage)
+  ) {
+    return false;
+  }
+
+  // 숫자인지 확인한다
+  const pages = [pobiLeftPage, pobiRightPage, crongLeftPage, crongRightPage];
+  for (let i = 0; i < pages.length; i++) {
+    if (!isNumber(pages[i])) return false;
+  }
+
+  // 오른쪽 페이지가 왼쪽 페이지 숫자보다 큰지 확인한다.
+  if (
+    !isValidPageNumber(pobiLeftPage, pobiRightPage) ||
+    !isValidPageNumber(crongLeftPage, crongRightPage)
+  ) {
+    return false;
+  }
+
+  // 페이지의 차이가 1을 초과하는지 확인한다.
+  if (
+    !isValidDifferences(pobiLeftPage, pobiRightPage) ||
+    !isValidDifferences(crongLeftPage, crongRightPage)
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
 const checkOdd = (leftPage) => {
   if (leftPage % 2) {
     return true;
@@ -38,8 +93,21 @@ const isOutOfRangePages = (leftPage, rightPage) => {
   return false;
 };
 
-//
+const isValidPageNumber = (leftpage, rightPage) => {
+  if (leftpage < rightPage) {
+    return true;
+  }
+  return false;
+};
 
+const isValidDifferences = (leftPage, rightPage) => {
+  if (rightPage - leftPage > 1) {
+    return false;
+  }
+  return true;
+};
+
+// 정상 페이지인 경우 사용될 함수
 const addSplitPageNumber = (page) => {
   const toStringPage = String(page);
   return toStringPage
@@ -56,20 +124,6 @@ const multiplySplitPageNumber = (page) => {
     .reduce((prev, current) => prev * current);
 };
 
-const isValidDifferences = (leftPage, rightPage) => {
-  if (rightPage - leftPage > 1) {
-    return false;
-  }
-  return true;
-};
-
-const compareLargerNumber = (addSplitNumber, multiplySplitNumber) => {
-  if (addSplitNumber > multiplySplitNumber) {
-    return addSplitNumber;
-  }
-  return multiplySplitNumber;
-};
-
 const compareScore = (pobiNumber, crongNumber) => {
   if (pobiNumber > crongNumber) {
     return 1;
@@ -81,6 +135,7 @@ const compareScore = (pobiNumber, crongNumber) => {
 };
 
 console.log(problem1([97, 98], [197, 198]));
+// console.log(problem1([0, 98], [197, 198])); // -1
 console.log(problem1([131, 132], [211, 212]));
 console.log(problem1([99, 102], [211, 212]));
 
