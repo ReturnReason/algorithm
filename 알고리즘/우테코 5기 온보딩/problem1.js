8e60c90b71372bc565f6801d5d492d66d84b6b14
ReturnReason <reasonz@daum.net>
2022-11-24 18:18:43 +0900

    feat: 페이지가 비어있는지 확인하는 함수 추가

--- 알고리즘/우테코 5기 온보딩/problem1.js
+++ 알고리즘/우테코 5기 온보딩/problem1.js
@@ -17,6 +17,13 @@ const checkEven = (rightPage) => {
   return false;
 };
 
+const isEmptyPages = ([leftPage, rightPage]) => {
+  if (!leftPage || !rightPage) {
+    return true;
+  }
+  return false;
+};
+
 console.log(problem1([97, 98], [197, 198]));
 console.log(problem1([131, 132], [211, 212]));
 console.log(problem1([99, 102], [211, 212]));
