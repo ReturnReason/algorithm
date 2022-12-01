8f9d1ad53fd7d6036e97b6ac9329805d80fd9aa4
ReturnReason <reasonz@daum.net>
2022-12-01 17:27:12 +0900

    feat(problem6): 이메일 형식이 올바르지 않는 경우 에러를 발생시키는 코드 추가

--- 알고리즘/우테코 5기 온보딩/problem6.js
+++ 알고리즘/우테코 5기 온보딩/problem6.js
@@ -1,8 +1,20 @@
+const isValidEmail = (email) => {
+  if (email.split('@')[1] === 'email.com') {
+    throw new Error('이메일 형식이 올바르지 않습니다.');
+  }
+};
+
+const isValidNickname = (nickname) => {
+  // 한글이 아닌 경우
+};
+
 const problem6 = (forms) => {
   const crew = {};
   const duplicateResult = [];
 
   forms.forEach(([email, nickname]) => {
+    isValidEmail(email);
+
     if (!crew[email]) {
       crew[email] = [];
     }
@@ -44,5 +56,15 @@ const problem6 = (forms) => {
 
   return [...new Set(duplicateResult)].sort();
 };
+
+console.log(
+  problem6([
+    ['jm@email.com', '제이엠'],
+    ['jason@email.com', '제이슨'],
+    ['woniee@email.com', '워니'],
+    ['mj@email.com', '엠제이'],
+    ['nowm@email.com', '이제엠'],
+  ])
+);
 
 module.exports = problem6;
