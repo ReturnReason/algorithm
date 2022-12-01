d65b74b343f9ce4d04b9179263ba7e4d714cc785
ReturnReason <reasonz@daum.net>
2022-12-01 16:50:39 +0900

    feat(problem7): 사용자의 타임라인에 방문한 횟수 1회당 1점을 부여하는 기능 구현

--- 알고리즘/우테코 5기 온보딩/problem7.js
+++ 알고리즘/우테코 5기 온보딩/problem7.js
@@ -1,7 +1,4 @@
 const problem7 = (user, friends, visitors) => {
-  // 프렌즈 배열에서 user와 아는 사람 목록을 추려낸다.
-  // 유저와 아는 사람 목록에 해당하는 모르는 사람이면 + 10점을 부여한다.
-
   let knowUsers = [];
   friends.forEach((friend) => {
     if (friend.includes(user)) {
@@ -39,6 +36,14 @@ const problem7 = (user, friends, visitors) => {
       score[firstUser]
     ) {
       score[firstUser] += 10;
+    }
+  });
+
+  visitors.forEach((visitor) => {
+    if (!knowUsers.includes(visitor) && !score[visitor]) {
+      score[visitor] = 1;
+    } else if (!knowUsers.includes(visitor) && score[visitor]) {
+      score[visitor] += 1;
     }
   });
 
